8f5b9dc81ffc90c7226b67d4928570361b3218e8
Denys Stoianov <denys.stoianov@raisin.com>
2019-12-16 12:30:49 +0100

    minot update printing unifi versions

--- src/test/groovy/ubiquiti/UnifiTest.groovy
+++ src/test/groovy/ubiquiti/UnifiTest.groovy
@@ -20,25 +20,22 @@ curl -s -X GET 'https://www.ui.com/download/?platform=unifi' \
 
 
     @Test
-    void printAwailableSoftwareVersionsOnServer() {
+    void printAvailableSoftwareVersionsOnServer() {
         def body = host.toURL().getText(requestProperties: prop)
         def parsedJson = new JsonSlurper().parseText(body)['downloads']
         def software = parsedJson.findAll { it.category__slug == 'software' }.sort { it.date_published }.reverse()
         def firmware = parsedJson.findAll { it.category__slug == 'firmware' }.sort { it.date_published }.reverse()
 
-
-        println "--software--"
-        printMy(software.findAll { it['description'].contains("Ubuntu") })
-        println "--firmware--"
-        printMy(firmware.findAll { it['products'].contains("AC-PRO") })
+        printMy(software.findAll { it['description'].contains("Ubuntu") }, "--software--")
+        printMy(firmware.findAll { it['products'].contains("AC-PRO") }, "--firmware--")
     }
 
 
-    static void printMy(def values, boolean prettyPrint = false) {
+    static void printMy(def values, def message, boolean prettyPrint = false) {
         def output = new JsonOutput()
-
+        println("=========" * 10)
+        println message
         for (def t : values) {
-            println("=========" * 10)
             println("${t.date_published} - ${t.version} - ${t.name}")
             if (prettyPrint) {
                 def jsonString = output.toJson(t)
